e0bcdfb8c4ba83f219cdfae73ea8928b2e705636
Elias Lezcano <eliaslezcano001@gmail.com>
2023-09-09 18:43:53 -0400

    T3 Stack initial setup with ChakraUI integration

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
