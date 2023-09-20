2f0c934c609862950ccd978fe1714cc7c1c0d2f1
Elias Lezcano <eliaslezcano001@gmail.com>
2023-09-20 11:08:11 -0400

    feat: Add error feedback elements to StandardInput

--- src/components/ui/StandardInput.tsx
+++ src/components/ui/StandardInput.tsx
@@ -1,16 +1,19 @@
-import { FormControl, FormLabel, Input, type InputProps } from "@chakra-ui/react";
+import { FormControl, FormErrorMessage, FormLabel, Input, type InputProps } from "@chakra-ui/react";
+import { type ForwardRefRenderFunction, forwardRef } from "react";
 import { twMerge } from 'tailwind-merge'
 
 interface StandardInputProps extends InputProps {
     label?: string;
     containerClassName?: HTMLDivElement["className"];
+    isError?: string;
 }
 
-export function StandardInput({ label, containerClassName = "", ...rest }: StandardInputProps) {
+const BaseStandardInput: ForwardRefRenderFunction<HTMLInputElement, StandardInputProps> = ({ label, containerClassName = "", isError, ...rest }, ref) => {
     return (
         <FormControl
             className={twMerge(`
                 w-full`, containerClassName)}
+            isInvalid={Boolean(isError)}
         >
             {label ? (
                 <FormLabel>
@@ -18,11 +21,19 @@ export function StandardInput({ label, containerClassName = "", ...rest }: Stand
                 </FormLabel>
             ) : null}
             <Input
+                ref={ref}
                 variant="filled"
                 className="
-                    !bg-gray-200"
+                !bg-gray-200"
                 {...rest}
             />
+            {isError ? (
+                <FormErrorMessage>
+                    {isError}
+                </FormErrorMessage>
+            ) : null}
         </FormControl>
     )
 }
+
+export const StandardInput = forwardRef(BaseStandardInput)
