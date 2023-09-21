152ea31980c4ba8b59c481d22fba2b7e279fdf47
Elias Lezcano <eliaslezcano001@gmail.com>
2023-09-21 19:08:54 -0400

    feat: Add teachers listing section

---
+++ src/components/TeachersTable.tsx
@@ -0,0 +1,86 @@
+import { Button, Flex, Spinner, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { api } from "~/utils/api";
+
+export function TeachersTable() {
+    const participants = api.participants.getAll.useQuery({
+        participantType: 'TEACHER'
+    })
+
+    return (
+        <TableContainer>
+            <Table variant='simple'>
+                <Thead>
+                    <Tr>
+                        <Th
+                            className="
+                                w-[20%]"
+                        >
+                            DOC DE IDENTIDAD
+                        </Th>
+                        <Th>NOMBRE Y APELLIDO</Th>
+                        <Th isNumeric>ACCIONES</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {participants.isFetching ? (
+                        <Tr>
+                        <Td
+                            colSpan={3}
+                        >
+                            <Flex
+                                className="
+                                    py-12
+                                    w-full
+                                    justify-center
+                                    gap-6
+                                    text-gray-600"
+                            >
+                                <Text>Cargando lista de profesores</Text>
+                                <Spinner />
+                            </Flex>
+                        </Td>
+                    </Tr>
+                    ) : (
+                        participants?.data && participants.data.length > 0 ? (participants.data.map(participant => (
+                            <Tr
+                                key={participant.CI}
+                            >
+                                <Td>{participant.CI}</Td>
+                                <Td>{participant.firstname + ' ' + participant.lastname}</Td>
+                                <Td isNumeric>
+                                    <Flex
+                                        className="
+                                            gap-2.5
+                                            justify-end"
+                                    >
+                                        <Button
+                                            colorScheme="orange"
+                                        >
+                                            Imprimir Acreditación
+                                        </Button>
+                                    </Flex>
+                                </Td>
+                            </Tr>
+                        ))) : (
+                            <Tr>
+                                <Td
+                                    colSpan={3}
+                                >
+                                    <Flex
+                                        className="
+                                            w-full
+                                            py-10
+                                            justify-center
+                                            text-gray-600"
+                                    >
+                                        No hay profesores registrados
+                                    </Flex>
+                                </Td>
+                            </Tr>
+                        )
+                    )}
+                </Tbody>
+            </Table>
+        </TableContainer>
+    )
+}
--- src/pages/profesores.tsx
+++ src/pages/profesores.tsx
@@ -1,9 +1,35 @@
+import { Button, Flex, Heading } from "@chakra-ui/react";
 import { DashboardLayout } from "~/components/layouts/DashboardLayout";
+import { Card } from "~/components/ui/Card";
+import NextLink from 'next/link';
+import { TeachersTable } from "~/components/TeachersTable";
 
 export default function Teachers() {
     return (
         <DashboardLayout>
-            
+            <Card
+                className="flex-col"
+            >
+                <Flex
+                    className="
+                        flex
+                        items-center
+                        justify-between
+                        w-full
+                        mb-12"
+                >
+                    <Heading
+                        as="h1"
+                        className="
+                            !text-2xl
+                            text-gray-600"
+                    >
+                        Listado de Profesores
+                    </Heading>
+                </Flex>
+                <TeachersTable />
+                {/* Pagination */}
+            </Card>
         </DashboardLayout>
     )
 }
