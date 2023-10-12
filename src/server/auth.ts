e228ddb5b8d75d200e1a3d681739cbd8411d22eb
Elias Lezcano <eliaslezcano001@gmail.com>
2023-10-12 01:32:35 -0300

    feat: Update the expiration of accessToken
    Added new comments describing accessToken expiration bug, and setted
    securedCookies

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -74,19 +74,30 @@ export const authOptions: NextAuthOptions = {
       session.error = token.error
       return session
     },
-
-    // Check this function for the bug
-    // The current bug is that the token is expired before that is intended, and
-    // seems to be no refreshed at all.
+    
+    /*
+      The bug was that in a time of one hour the token expires, first i don't know what. The thing was that in the session
+      property i have an object with two properties: strategy and maxAge. The maxAge basically defines the difference
+      between the iat and exp values which are in the JWT token. So, when i use the function the decode function in my utils,
+      from the next-auth it throws an error if the exp token says that the token expired, and the exp is defined based
+      on the maxAge param. In my implementation i have inside the object the property accessTokenExpired.
+    */
+    // ---
+    // This is only executed when the user sends a token
+    // Something  strange is that the user and values other than the token are undefined
+    // This callback is executed everytime the user makes makes a request to a protected route.
+    // In the first call to this callback the user key of the object param have a value, then when a token is sended,
+    // don't have a new value, i don't know now why
     async jwt({ token, user }) {
+      // The refresh token is not refreshed automatically, when expired, then the user should signin
       if (user) {
         const authUser = { participantCI: user.participantCI, id: user.id, institutionISO: user.institutionISO, institutionAbbreviation: user.institutionAbbreviation};
 
         const accessToken = await jwtHelper.createAccessToken(authUser)
         const refreshToken = await jwtHelper.createRefreshToken(authUser)
 
-        const accessTokenExpired = Date.now() / 1000 + tokenOneDay;
-        const refreshTokenExpired = Date.now() / 1000 + tokenOneWeek;
+        const accessTokenExpired = Date.now() + tokenOneDay;
+        const refreshTokenExpired = Date.now() + tokenOneWeek;
 
         return {
           ...token,
@@ -98,8 +109,7 @@ export const authOptions: NextAuthOptions = {
         }
       } else {
         if (token) {
-          // In subsequent requests, check access token has expired, try to refresh it
-          if (Date.now() / 1000 > token.accessTokenExpired!) {
+          if (Date.now() > token.accessTokenExpired!) {
             const verifyToken = await jwtHelper.verifyToken(token.refreshToken!)
 
             if (verifyToken) {
@@ -110,8 +120,11 @@ export const authOptions: NextAuthOptions = {
               })
 
               if (user) {
-                const accessToken = await jwtHelper.createAccessToken(token.user!)
-                const accessTokenExpired = Date.now() / 1000 * tokenOneDay
+                const accessToken = await jwtHelper.createAccessToken({
+                  ...token.user!
+                })
+
+                const accessTokenExpired = Date.now() * tokenOneDay
 
                 return {
                   ...token,
@@ -137,9 +150,9 @@ export const authOptions: NextAuthOptions = {
   secret: env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
-    maxAge: 60 * 60 // 1 hour
+    maxAge: 60 * 60 * 24 // 1 day
   },
-  // useSecureCookies: true,
+  useSecureCookies: process.env.NODE_ENV === "production",
   pages: {
     signIn: '/auth/login',
   },
@@ -176,7 +189,6 @@ export const authOptions: NextAuthOptions = {
 
           if (user && credentials) {
             const validPassword = await bcrypt.compare(password, user.password)
-            console.log(validPassword)
 
             if (validPassword) {
               return {
--- src/utils/jwtHelper.ts
+++ src/utils/jwtHelper.ts
@@ -4,11 +4,13 @@ import { type User } from '@prisma/client'
 
 export type AuthUser = Omit<User, "image" | "password" | "emailVerified" | "email" | "name"> & {institutionISO: number, institutionAbbreviation: string};
 
-export const tokenOneDay = 24 * 60 * 60
+export const tokenOneDay = 24 * 60 * 60 * 1000 // in milliseconds
 export const tokenOneWeek = tokenOneDay * 7
 
 // The problem i have is that the type of token is affected by the override of the JWT type in my
 // auth file, so what i will do is to solve this problem temporarily is to set all the properties to optional
+
+// The decode function will throw if the sended token is invalid
 
 const createJWT = (payload: AuthUser, duration: number) => encode({ token: payload , secret: env.JWT_SECRET, maxAge: duration })
 
